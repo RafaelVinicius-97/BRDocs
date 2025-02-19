c759e716da9e9ca10bea891ec1ea6a5262f071ae
Rafael Vinícius <RafaelVinicius.97@outlook.com>
2025-02-19 10:41:11 -0300

    feat: cnpj refactor: cpf

--- BRDocs.Lib/CNPJ.cs
+++ BRDocs.Lib/CNPJ.cs
@@ -2,24 +2,21 @@
 
 namespace BRDocs.Lib;
 
-public static class CPF
+public class CNPJ
 {
-    private static readonly int _tamanhoDocumento = 11;
-    private static readonly int[] _multiplicadoresSemDigitoVerificador = [10, 9, 8, 7, 6, 5, 4, 3, 2];
-    private static readonly int[] _multiplicadoresComUmDigitoVerificador = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+    private static readonly int _tamanhoDocumento = 14;
+    private static readonly int[] _multiplicadoresSemDigitoVerificador = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+    private static readonly int[] _multiplicadoresComUmDigitoVerificador = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
 
     public static bool Validar(string documento)
     {
         documento = RemoverCaracteresEspeciais(documento);
 
-        if (!DigitosEstaoValidos(documento))
+        if (DigitosEstaoValidos(documento) is false)
             return false;
 
         return DigitosVerificadoresValidos(documento);
     }
-
-    private static string RemoverCaracteresEspeciais(string documento) =>
-        documento.Trim().Replace(".", "").Replace("-", "");
 
     private static bool DigitosEstaoValidos(string documento)
     {
@@ -29,16 +26,23 @@ public static class CPF
         return documento.Length == _tamanhoDocumento;
     }
 
+    private static string RemoverCaracteresEspeciais(string documento) =>
+    documento
+        .Trim()
+        .Replace(".", "")
+        .Replace("-", "")
+        .Replace("/", "");
+
     private static bool DigitosVerificadoresValidos(string documento)
     {
-        var digitoVerificadorCalculado = CalcularDigitoVerificador(documento, tamanhoDocumento: 9, _multiplicadoresSemDigitoVerificador);
+        var digitoVerificadorCalculado = CalcularDigitoVerificador(documento, tamanhoDocumento: 12, _multiplicadoresSemDigitoVerificador);
 
-        if (documento[9] != digitoVerificadorCalculado.ToString()[0])
+        if (documento[12] != digitoVerificadorCalculado.ToString()[0])
             return false;
 
-        digitoVerificadorCalculado = CalcularDigitoVerificador(documento, tamanhoDocumento: 10, _multiplicadoresComUmDigitoVerificador);
+        digitoVerificadorCalculado = CalcularDigitoVerificador(documento, tamanhoDocumento: 13, _multiplicadoresComUmDigitoVerificador);
 
-        if (documento[10] != digitoVerificadorCalculado.ToString()[0])
+        if (documento[13] != digitoVerificadorCalculado.ToString()[0])
             return false;
 
         return true;
@@ -46,12 +50,14 @@ public static class CPF
         static int CalcularDigitoVerificador(string documento, int tamanhoDocumento, int[] multiplicadores)
         {
             var soma = 0;
+
             for (var index = 0; index < tamanhoDocumento; index++)
             {
                 soma += (documento[index] - '0') * multiplicadores[index];
             }
 
             var resto = soma % 11;
+
             return resto < 2 ? 0 : 11 - resto;
         }
     }
